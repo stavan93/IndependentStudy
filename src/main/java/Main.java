5256aded09ebac5cf3f2249eac7489072f019c7e
Stavan Anjaria <spa1019@wildcats.unh.edu>
2020-07-17 19:11:19 -0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        MinIE_Triples min = new MinIE_Triples();
+        min.extract();
+    }
+}
